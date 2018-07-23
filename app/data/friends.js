25d9952034b1e437a3c6456b0c49dc8c013d3b09
bkell11 <bkeller11@msn.com>
2018-07-23 15:48:18 -0600

    1 object

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,21 @@
-var friendData = [];
+var friendData = [
+    {
+        name: "Blake",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOefN7nxEi2qsN6cv8V4RqliERcDH6zmtlYyJbgWDeuGbRNzS_",
+        scores: [
+            5,
+            5,
+            2,
+            5,
+            5,
+            4,
+            4,
+            4,
+            5,
+            5
+        ]
+
+    }
+];
 
 module.exports = friendData;
